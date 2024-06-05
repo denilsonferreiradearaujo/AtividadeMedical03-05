c7fda835993f502c91839145f0e4b9659f5266cd
Gabriel Pantalhão <pantalhaog@gmail.com>
2024-06-04 21:46:02 -0300

    Atualização do controller e da ClienteModel 04/06/2024

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,20 +4,12 @@ const router = express.Router();
 const Controller = require("../Controllers/clienteController");
 
 router.post('/cliente', Controller.adicionarCliente);
+router.put('/cliente/:cpf', Controller.atualizarCliente);
+router.delete('/cliente/:cpf', Controller.deletarCliente);
+router.get('/clientes', Controller.exibirTodos);
+router.get('/cliente/:cpf', Controller.exibirCliente);
 
 // router.get("/", Controller.index)
-
-// router.get("/cadastro", Controller.cadastro)
-
-// router.get("/listar", Controller.listar);
-
-// router.get('/editar/:id', Controller.editar);
-
-// router.post('/editar/usuario', Controller.salvarEdicao);
-
-// router.get('/:id', Controller.selecionar);
-
-// router.delete('/excluir/:id', Controller.deletarUsuario);
 
 // router.use(function(req, res){
 //     res.status(404).render(`pages/pag_erro`, {message:'404 - Página não encontrada'})
